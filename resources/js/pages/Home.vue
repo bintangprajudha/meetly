b97b62b07742283f939ad9f350456aa4ef5643e7
Farazell <152409066+Farazell@users.noreply.github.com>
2025-12-08 11:15:20 +0700

    Update Home.vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -60,7 +60,7 @@ import { Link } from '@inertiajs/vue3';
                 </div>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                     <Link
-                        href="{{ route('login') }}"
+                        href="{{ route('show.login') }}"
                         class="text-sm/6 font-semibold text-white"
                         >Log in <span aria-hidden="true">&rarr;</span></Link
                     >
